aaa3274b1e3048516e32ca5dde1046f7a2e568f8
Ryandinulfatah12 <ryandfbasketball12@gmail.com>
2020-06-26 12:32:25 +0700

    first

---
+++ dist/js/custom.js
@@ -0,0 +1,21 @@
+// Team 
+const team = [
+    {
+        img: "dist/images/team/rayen.png",
+        description: "Lorem ipsum dolor sit amet",
+        username: "@sagiratisandy",
+        link: "https://www.instagram.com/sagitarisandy/"
+    },
+    {
+        img: "dist/images/team/kopeng.png",
+        description: "Lorem ipsum dolor sit amet",
+        username: "@sagiratisandy",
+        link: "https://www.instagram.com/sagitarisandy/"
+    },
+    {
+        img: "dist/images/team/novi.png",
+        description: "Lorem ipsum dolor sit amet",
+        username: "@sagiratisandy",
+        link: "https://www.instagram.com/sagitarisandy/"
+    }
+];
